6c0eed5085f4a9b9c4f19e394563d64baf5eec23
ONATE VILLAVERDE Pablo Javier <pablo-javier.onate@soprasteria.com>
2019-11-12 21:55:34 +0100

    Primera versión del módulo de matemáticas

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+    /**
+     * Suma de dos números
+     * 
+     * @example 
+     * @param {*} a 
+     * @param {*} b 
+     */
+    suma: function(a,b) {
+        return a + b;
+    },
+
+    /**
+     * Resta de dos números
+     * 
+     * @param {*} a 
+     * @param {*} b 
+     */
+    resta: function(a,b) {
+        return a - b;
+    },
+
+    /**
+     * Multiplicación de dos números
+     * 
+     * @param {*} a 
+     * @param {*} b 
+     */
+    multiplicacion: function(a,b) {
+        return a * b;
+    },
+
+    /**
+     * División de dos números
+     * 
+     * @param {*} a 
+     * @param {*} b 
+     */
+    division: function(a,b) {
+        return a / b;
+    }
+}
